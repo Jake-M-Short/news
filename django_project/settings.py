c39a8b086d70d0c6760dc2e4a008a88d5118d30a
Jake Short <jakemshort@yahoo.com>
2023-10-31 07:12:07 -0600

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #Local
     "accounts",
     "pages",
+    "articles",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
